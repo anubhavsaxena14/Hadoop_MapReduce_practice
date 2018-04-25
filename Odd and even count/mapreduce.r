ea2d7abbbb6a5b281cad44e177e59b3301c391ea
Anubhav Saxena <anubhavsaxena14992@gmail.com>
2018-04-25 21:11:30 +0530

    Create mapreduce.r

---
+++ Odd and even count/mapreduce.r
@@ -0,0 +1,12 @@
+numbers=c(1:50)
+dfs.numbers=to.dfs(numbers)
+funcMap=function(k,v){
+key=ifelse(v%%2==0,”Even”,”Odd”);
+return(keyval(key,v));
+}
+funcReduce=function(k,v){
+val=length(v);
+return(keyval(k,val));
+}
+mpres=mapreduce(dfs.numbers,map=funcMap,reduce=funcReduce)
+from.dfs(mpres)
