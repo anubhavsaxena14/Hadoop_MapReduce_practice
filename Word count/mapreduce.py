eacf32ecde63ec356cde44e407748d65530ef00c
Anubhav Saxena <anubhavsaxena14992@gmail.com>
2018-05-04 00:12:44 +0530

    Update

--- Word count/mapreduce.py
+++ Word count/mapreduce.py
@@ -1,1 +1,12 @@
-
+funcMap=function(k,v){
+  words.list  = strsplit(v," ")
+  words = unlist(words.list)
+  return(keyval(words,1))
+}
+funcReduce=function(word,count){
+  return(keyval(word,sum(count)))
+}
+mp=mapreduce(input = "/Hadoop_Examples/wordcount.txt", input.format = "text", map=funcMap,reduce=funcReduce)
+from.dfs(mp)
+as.data.frame(result)
+result
